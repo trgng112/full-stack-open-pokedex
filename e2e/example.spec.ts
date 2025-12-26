b6ea2e995bcf2b8dc654529b1f36ae1ffbd60e28
Truong Nguyen <scythe.descin@gmail.com>
2025-12-26 12:56:12 +0700

    add navigation test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -6,4 +6,11 @@ test.describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
